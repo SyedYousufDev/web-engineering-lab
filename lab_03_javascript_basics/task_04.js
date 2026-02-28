8cb629967373cb8f4be1dbbd83e939feb443439b
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-28 22:43:07 +0500

    Added 4th task which find sum and average of three numbers:

---
+++ lab_03_javascript_basics/task_04.js
@@ -0,0 +1,14 @@
+
+  // Prompt user for 3 integers
+  let num1 = Number(prompt("Enter the first integer:"));
+  let num2 = Number(prompt("Enter the second integer:"));
+  let num3 = Number(prompt("Enter the third integer:"));
+
+  // Calculate Sum
+  let sum = num1 + num2 + num3;
+
+  // Calculate Average
+  let average = sum / 3;
+
+  // Return results in a dialogue box (alert)
+  alert("Sum: " + sum + "\nAverage: " + average);
