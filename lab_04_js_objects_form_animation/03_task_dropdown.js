5783d73692cf805019e34e09589371e605a29ee1
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-03-07 18:06:51 +0500

    Added 3rd task which create a dropdown and add a option at runtime

---
+++ lab_04_js_objects_form_animation/03_task_dropdown.js
@@ -0,0 +1,25 @@
+const dropdown = document.getElementById("myDropdown");
+const textbox = document.getElementById("myTextbox");
+const button = document.getElementById("newEntryBtn");
+
+function showTextbox() {
+    button.style.display = "none";        
+    dropdown.style.display = "none";      
+    textbox.style.display = "inline";     
+    textbox.focus();                       
+}
+
+
+textbox.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        const value = textbox.value.trim();
+        if (value) {
+            const option = document.createElement("option");
+            option.text = value;
+            dropdown.add(option);        
+            textbox.value = "";           
+            textbox.style.display = "none"; 
+            dropdown.style.display = "inline"; 
+        }
+    }
+});
